28b2df895cc7b16aafe3aa7f89dcc774db867447
kai.wang <kai.wang.john@gmail.com>
2014-01-21 08:33:37 +0800

    format code

--- src/com/painting/MainActivity.java
+++ src/com/painting/MainActivity.java
@@ -3,130 +3,124 @@ package com.painting;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
-import android.graphics.Point;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.WindowManager;
 
 import com.painting.component.Brush;
 
 public class MainActivity extends Activity {
 
-	DrawingView dv;
-	private Paint mPaint;
+    DrawingView dv;
+    private Paint mPaint;
 
-	private int COLOR_BLUE_LIGHT = 0xFF00C4D2;
+    private int COLOR_BLUE_LIGHT = 0xFF00C4D2;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		dv = new DrawingView(this);
-		dv.setBackgroundColor(0xFF130527);
-		setContentView(dv);
-		mPaint = new Paint();
-		mPaint.setAntiAlias(true);
-		mPaint.setDither(true);
-		mPaint.setColor(COLOR_BLUE_LIGHT);
-		mPaint.setStyle(Paint.Style.STROKE);
-		mPaint.setStrokeJoin(Paint.Join.ROUND);
-		mPaint.setStrokeCap(Paint.Cap.ROUND);
-		mPaint.setStrokeWidth(1);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        dv = new DrawingView(this);
+        dv.setBackgroundColor(0xFF130527);
+        setContentView(dv);
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setDither(true);
+        mPaint.setColor(COLOR_BLUE_LIGHT);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeJoin(Paint.Join.ROUND);
+        mPaint.setStrokeCap(Paint.Cap.ROUND);
+        mPaint.setStrokeWidth(1);
+    }
 
-	public class DrawingView extends View {
+    public class DrawingView extends View {
 
-		public int width;
-		public int height;
-		private Bitmap mBitmap;
-		private Canvas mCanvas;
-		private Path mPath;
-		private Paint mBitmapPaint;
-		Context context;
+        public int width;
+        public int height;
+        private Bitmap mBitmap;
+        private Canvas mCanvas;
+        private Path mPath;
+        private Paint mBitmapPaint;
+        Context context;
 
-		private Brush brush;
+        private Brush brush;
 
-		public DrawingView(Context c) {
-			super(c);
-			context = c;
-			mPath = new Path();
-			mBitmapPaint = new Paint(Paint.DITHER_FLAG);
-		}
+        public DrawingView(Context c) {
+            super(c);
+            context = c;
+            mPath = new Path();
+            mBitmapPaint = new Paint(Paint.DITHER_FLAG);
+        }
 
-		@Override
-		protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-			super.onSizeChanged(w, h, oldw, oldh);
+        @Override
+        protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+            super.onSizeChanged(w, h, oldw, oldh);
 
-			mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
-			mCanvas = new Canvas(mBitmap);
-			brush = new Brush(context,mCanvas);
-		}
+            mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+            mCanvas = new Canvas(mBitmap);
+            brush = new Brush(context, mCanvas);
+        }
 
-		@Override
-		protected void onDraw(Canvas canvas) {
-			super.onDraw(canvas);
+        @Override
+        protected void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
 
-			canvas.drawBitmap(mBitmap, 0, 0, mBitmapPaint);
+            canvas.drawBitmap(mBitmap, 0, 0, mBitmapPaint);
 
-			brush.drawPath(canvas, mPaint);
+            brush.drawPath(canvas, mPaint);
 
-			brush.drawCursor(canvas, mX, mY);
-		}
+            brush.drawCursor(canvas, mX, mY);
+        }
 
-		private float mX, mY;
-		private static final float TOUCH_TOLERANCE = 4;
+        private float mX, mY;
+        private static final float TOUCH_TOLERANCE = 4;
 
-		private void touch_start(float x, float y) {
-			brush.reset();
-			brush.moveTo(x, y);
-			mX = x;
-			mY = y;
-		}
+        private void touch_start(float x, float y) {
+            brush.reset();
+            brush.moveTo(x, y);
+            mX = x;
+            mY = y;
+        }
 
-		private void touch_move(float x, float y) {
-			float dx = Math.abs(x - mX);
-			float dy = Math.abs(y - mY);
-			if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
-				brush.quadTo(mX, mY, x, y);
-				mX = x;
-				mY = y;
-			}
-		}
+        private void touch_move(float x, float y) {
+            float dx = Math.abs(x - mX);
+            float dy = Math.abs(y - mY);
+            if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
+                brush.quadTo(mX, mY, x, y);
+                mX = x;
+                mY = y;
+            }
+        }
 
-		private void touch_up() {
-			brush.lineTo(mX, mY);
-			// commit the path to our offscreen
-			brush.drawPath(mCanvas, mPaint);
-			// kill this so we don't double draw
-			brush.reset();
-		}
+        private void touch_up() {
+            brush.lineTo(mX, mY);
+            // commit the path to our offscreen
+            brush.drawPath(mCanvas, mPaint);
+            // kill this so we don't double draw
+            brush.reset();
+        }
 
-		@Override
-		public boolean onTouchEvent(MotionEvent event) {
-			float x = event.getX();
-			float y = event.getY();
-			switch (event.getAction()) {
-				case MotionEvent.ACTION_DOWN:
-					touch_start(x, y);
-					invalidate();
-					break;
-				case MotionEvent.ACTION_MOVE:
-					touch_move(x, y);
-					invalidate();
-					break;
-				case MotionEvent.ACTION_UP:
-					touch_up();
-					invalidate();
-					break;
-			}
-			return true;
-		}
-	}
+        @Override
+        public boolean onTouchEvent(MotionEvent event) {
+            float x = event.getX();
+            float y = event.getY();
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    touch_start(x, y);
+                    invalidate();
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    touch_move(x, y);
+                    invalidate();
+                    break;
+                case MotionEvent.ACTION_UP:
+                    touch_up();
+                    invalidate();
+                    break;
+            }
+            return true;
+        }
+    }
 }
--- src/com/painting/component/Brush.java
+++ src/com/painting/component/Brush.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Point;
@@ -18,66 +17,66 @@ import com.painting.R;
  */
 public class Brush {
 
-	public int width;
-	public int height;
-	private Bitmap pointBitmap;
-	private Bitmap lightingBitmap;
+    public int width;
+    public int height;
+    private Bitmap pointBitmap;
+    private Bitmap lightingBitmap;
 
-	private Context context;
-	private Canvas canvas;
+    private Context context;
+    private Canvas canvas;
 
-	private Path pathA, pathB;
+    private Path pathA, pathB;
 
-	public Brush(Context context,Canvas canvas) {
-		this.context = context;
-		this.canvas = canvas;
-		WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-		Display display = wm.getDefaultDisplay();
-		Point size = new Point();
-		display.getSize(size);
-		width = size.x;
-		height = size.y;
+    public Brush(Context context, Canvas canvas) {
+        this.context = context;
+        this.canvas = canvas;
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        width = size.x;
+        height = size.y;
 
-		pointBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.point_white);
-		lightingBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.lighting);
-		pathA = new Path();
-		pathB = new Path();
-	}
+        pointBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.point_white);
+        lightingBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.lighting);
+        pathA = new Path();
+        pathB = new Path();
+    }
 
-	public void quadTo(float prvX, float prvY, float x2, float y2) {
-		pathA.quadTo(prvX, prvY, (prvX + x2) / 2, (prvY + y2) / 2);
-		pathB.quadTo(width - prvX, prvY, (2*width - x2 - prvX)/2, (prvY + y2) / 2);
-		canvas.drawBitmap(pointBitmap, x2, y2, null);
-	}
+    public void quadTo(float prvX, float prvY, float x2, float y2) {
+        pathA.quadTo(prvX, prvY, (prvX + x2) / 2, (prvY + y2) / 2);
+        pathB.quadTo(width - prvX, prvY, (2 * width - x2 - prvX) / 2, (prvY + y2) / 2);
+//		canvas.drawBitmap(pointBitmap, x2, y2, null);
+    }
 
-	public void reset() {
-		pathA.reset();
-		pathB.reset();
-	}
+    public void reset() {
+        pathA.reset();
+        pathB.reset();
+    }
 
-	public void moveTo(float x, float y) {
-		pathA.moveTo(x, y);
-		pathB.moveTo(width - x, y);
-	}
+    public void moveTo(float x, float y) {
+        pathA.moveTo(x, y);
+        pathB.moveTo(width - x, y);
+    }
 
-	public void lineTo(float x, float y) {
-		pathA.lineTo(x, y);
-		pathB.lineTo(width - x, y);
-	}
+    public void lineTo(float x, float y) {
+        pathA.lineTo(x, y);
+        pathB.lineTo(width - x, y);
+    }
 
-	/**
-	 * 画光标（两个点）
-	 *
-	 * @param canvas
-	 */
-	public void drawCursor(Canvas canvas, float x, float y) {
-		canvas.drawBitmap(pointBitmap, x, y, null);
-		canvas.drawBitmap(pointBitmap, width - x, y, null);
-	}
+    /**
+     * 画光标（两个点）
+     *
+     * @param canvas
+     */
+    public void drawCursor(Canvas canvas, float x, float y) {
+        canvas.drawBitmap(pointBitmap, x, y, null);
+        canvas.drawBitmap(pointBitmap, width - x, y, null);
+    }
 
-	public void drawPath(Canvas canvas, Paint paint) {
-		canvas.drawPath(pathA, paint);
-		canvas.drawPath(pathB, paint);
-	}
+    public void drawPath(Canvas canvas, Paint paint) {
+        canvas.drawPath(pathA, paint);
+        canvas.drawPath(pathB, paint);
+    }
 
 }
